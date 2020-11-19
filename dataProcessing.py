b7202e9fb4bb756370a9a6b6b1f29645c0f7111e
victoriaaarmstrong <57503137+victoriaaarmstrong@users.noreply.github.com>
2020-11-18 20:04:07 -0500

    updated generateData method

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 def meanImputation(data):
     """
@@ -14,20 +15,12 @@ def meanImputation(data):
 
     return data
 
-
-def importCSV(fileName):
+def countMissing(participant):
     """
-    :param fileName: the name of the file to read in
-    :return: DataFrame object
+    Prints metrics for missing data for any given participant set
+    :param participant: participant DataFrame
+    :return: None
     """
-
-    ## Read in the csv file to DataFrame
-    participant = pd.read_csv(fileName)
-
-    ## Drop the unneccessary columns
-    participant = participant.drop(['Condition', 'C', 'Unnamed: 0', 'date', 'subject'], axis = 1)
-    ## TO DO
-    ## Do I need to keep the Elapsed time? I know they're all one minute average time stamps
 
     ## Initialize counters
     numHR = 0
@@ -55,7 +48,29 @@ def importCSV(fileName):
     print(" \t RMSSD missing: " + str(perRMSSD))
     print(" \t SCL missing: " + str(perSCL))
 
+    return
+
+def importCSV(fileName):
+    """
+    :param fileName: the name of the file to read in
+    :return: DataFrame object
+    """
+
+    ## Read in the csv file to DataFrame
+    participant = pd.read_csv(fileName)
+
+    ## Drop the unneccessary columns
+    ## !! Don't think I actually need to do this because feature extraction is just going to happen in the sliding window
+    ## participant = participant.drop(['Condition', 'C', 'Unnamed: 0', 'date', 'subject'], axis = 1)
+
+    ## Fill missing data
     participant = meanImputation(participant)
+
+    ## !! Normalize?
+    ## !! Could maybe do this in the tensor - there should be a function for this
+
+    ## To display missing metrics if you want
+    ##countMissing(participant)
 
     return participant
 
@@ -79,10 +94,57 @@ def plotFeature(participant, title):
 
     return
 
-
-def readAllData():
+def generateSamples(numTargets, df, windowSize):
     """
-    :return: None
+
+    :param numTargets:
+    :param df:
+    :param lenContext:
+    :param lenTarget:
+    :return:
+    """
+
+    if numTargets == 2:
+        d = {'rest':0, 'no stress':0, 'time pressure':1, 'interruption':1}
+    else:
+        d = {'rest':0, 'no stress':1, 'time pressure':2, 'interruption':3}
+
+    df = df.replace(d)
+
+
+    ## Initialize variables
+    feature1 = df['HR'].tolist()
+    feature2 = df['RMSSD'].tolist()
+    feature3 = df['SCL'].tolist()
+    targets = df['label'].tolist()
+
+    x = []
+    y = []
+
+    start = 0
+    end = windowSize
+    dataLength = len(df.index)
+
+    ## !! Really check that this is iterating through how you want it to, it might not be
+    while end < dataLength:
+        context = feature1[start:end] + feature2[start:end] + feature3[start:end]
+        x.append(context)
+        y.append(targets[start:end])
+        start += 1
+        end += 1
+
+    ## !! Maybe need to reshape y? not sure
+    return x, y
+
+
+def generateData(windowSize):
+    """
+    Returns a train, test and validation set of time series data
+    :param windowSize: how big you want the sliding window to be
+    :param trainSize: size of training set, default 0.7
+    :param testSize: size of testing set, default 0.15
+    :param validationSize: side of validation set, default 0.15
+    :return:
     """
 
     ## Read in participant data files
@@ -112,6 +174,21 @@ def readAllData():
     p24 = importCSV('p24.csv')
     p25 = importCSV('p25.csv')
 
-    return
+    participantDFs = [p1, p2, p3, p4, p5, p6, p7, p9, p10, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25]
 
-readAllData()
+    features = []
+    labels = []
+
+    for participant in participantDFs:
+        temp1, temp2 = generateSamples(2, participant, 3)
+        features = features + temp1
+        labels = labels + temp2
+
+    trainX, testX, trainY, testY = train_test_split(features, labels, test_size=0.15)
+
+    trainFeatures = np.array(trainX)
+    trainLabels = np.array(trainY)
+    testFeatures = np.array(testX)
+    testLabels = np.array(testY)
+
+    return trainFeatures, trainLabels, testFeatures, testLabels

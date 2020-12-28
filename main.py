bedbb21952e0bef86efaf0e1d6c7c7a89a21f69d
victoriaaarmstrong <57503137+victoriaaarmstrong@users.noreply.github.com>
2020-12-27 20:47:57 -0500

    final testing

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import tensorflow as tf
-import keras
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from keras.preprocessing.sequence import TimeseriesGenerator
-from sklearn.preprocessing import normalize, MinMaxScaler
 
+from tensorflow.keras.layers import Dense, LSTM, Reshape
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-
-from model import simpleLSTM
-
 
 def meanImputation(data):
     """
@@ -26,7 +22,7 @@ def meanImputation(data):
     return data
 
 
-def countMissing(participant):
+def countMissing(participant, fileName):
     """
     Prints metrics for missing data for any given participant set
     :param participant: participant DataFrame
@@ -71,18 +67,16 @@ def importCSV(fileName):
     ## Read in the csv file to DataFrame
     participant = pd.read_csv(fileName)
 
-    ## Drop the unneccessary columns
-    ## !! Don't think I actually need to do this because feature extraction is just going to happen in the sliding window
-    ## participant = participant.drop(['Condition', 'C', 'Unnamed: 0', 'date', 'subject'], axis = 1)
-
     ## Fill missing data
+    #participant['HR'] = participant['HR'].fillna(0) ## zero-insertion
     participant = meanImputation(participant)
 
-    ## !! Normalize?
-    ## !! Could maybe do this in the tensor - there should be a function for this
+    participant = participant.drop(['Unnamed: 0', 'C', 'timestamp', 'date', 'PP', 'ElapsedTime', 'Condition'], axis=1)
+    participant = participant[~participant['label'].isin(['rest'])]
+    participant = participant.replace({'no stress': 0, 'interruption': 1, 'time pressure': 2})
 
     ## To display missing metrics if you want
-    ##countMissing(participant)
+    ##countMissing(participant, fileName)
 
     return participant
 
@@ -94,7 +88,7 @@ def plotFeature(participant, title):
     :return: None
     """
     participant.plot(x='timestamp',
-                     y=['HR', 'RMSSD', 'SCL'],
+                     y=['HR'], #, 'RMSSD', 'SCL'],
                      kind='line',
                      title=title,
                      xlabel='Time',
@@ -107,87 +101,48 @@ def plotFeature(participant, title):
     return
 
 
-def generateSamples(numTargets, df, windowSize):
-    """
-
-    :param numTargets:
-    :param df:
-    :param lenContext:
-    :param lenTarget:
-    :return:
-    """
-
-    if numTargets == 2:
-        d = {'rest': 0, 'no stress': 0, 'time pressure': 1, 'interruption': 1}
-    else:
-        d = {'rest': 0, 'no stress': 1, 'time pressure': 2, 'interruption': 3}
-
-    df = df.replace(d)
-
-    ## Initialize variables
-    feature1 = df['HR'].tolist()
-    feature2 = df['RMSSD'].tolist()
-    feature3 = df['SCL'].tolist()
-    targets = df['label'].tolist()
-
-    x = []
-    y = []
-
-    start = 0
-    end = windowSize
-    dataLength = len(df.index)
-
-    ## !! Really check that this is iterating through how you want it to, it might not be
-    while end < dataLength:
-        context = feature1[start:end] + feature2[start:end] + feature3[start:end]
-        x.append(context)
-        y.append(targets[start:end])
-        start += 1
-        end += 1
-
-    ## !! Maybe need to reshape y? not sure
-    return x, y
-
-
 def timeSeries(data, windowSize, predictionSize):
-    min_max_scaler = MinMaxScaler()
+    #min_max_scaler = MinMaxScaler()
 
-    # x_scaled = min_max_scaler.fit_transform(x)
-
-    feature1 = data['HR'].values.reshape(-1, 1)
-    feature1 = min_max_scaler.fit_transform(feature1)
-
-    feature2 = data['RMSSD'].values.reshape(-1, 1)
-    feature2 = min_max_scaler.fit_transform(feature2)
-
-    feature3 = data['SCL'].values.reshape(-1, 1)
-    feature3 = min_max_scaler.fit_transform(feature3)
+    vals = data['subject'].unique()
 
     x = []
     y = []
 
-    start = 0
-    endC = windowSize
-    endT = windowSize + predictionSize
-    dataLength = len(data.index)
+    for v in vals:
+        participant = data.loc[data['subject'] == str(v)]
 
-    while endT <= dataLength:
-        t1 = feature1[start:endC]
-        t2 = feature2[start:endC]
-        t3 = feature2[start:endC]
-        context = np.concatenate((t1, t2, t3), axis=1)
+        feature1 = participant['HR'].values.reshape(-1, 1)
+        # feature1 = min_max_scaler.fit_transform(feature1) ## where it had been normalized
 
-        t4 = feature1[endC:endT]
-        t5 = feature2[endC:endT]
-        t6 = feature3[endC:endT]
-        target = np.concatenate((t4, t5, t6), axis=1)
+        feature2 = participant['RMSSD'].values.reshape(-1, 1)
+        # feature2 = min_max_scaler.fit_transform(feature2) ## where it had been normalized
 
-        x.append(context)
-        y.append(target)
+        feature3 = participant['SCL'].values.reshape(-1, 1)
+        # feature3 = min_max_scaler.fit_transform(feature3) ## where it had been normalized
 
-        start += 1
-        endC += 1
-        endT += 1
+        start = 0
+        endC = windowSize
+        endT = windowSize + predictionSize
+        dataLength = len(participant.index)
+
+        while endT <= dataLength:
+            t1 = feature1[start:endC]
+            t2 = feature2[start:endC]
+            t3 = feature2[start:endC]
+            context = np.concatenate((t1, t2, t3), axis=1)
+
+            t4 = feature1[endC:endT]
+            t5 = feature2[endC:endT]
+            t6 = feature3[endC:endT]
+            target = np.concatenate((t4, t5, t6), axis=1)
+
+            x.append(context)
+            y.append(target)
+
+            start += 1
+            endC += 1
+            endT += 1
 
     x = np.array(x)
     y = np.array(y)
@@ -195,19 +150,34 @@ def timeSeries(data, windowSize, predictionSize):
     return x, y
 
 
+def lstmTrain(windowLength, predictionSize, trainX, trainY):
+
+    model = Sequential()
+    model.add(LSTM(32, input_shape=(windowLength,3), return_sequences=False))
+    model.add(Dense(predictionSize*3, kernel_initializer=tf.initializers.zeros))
+    model.add(Reshape([predictionSize, 3]))
+
+    model.compile(optimizer='adam', loss=tf.keras.losses.MeanAbsoluteError(), metrics=[tf.keras.metrics.MeanAbsoluteError()])
+
+    model.fit(x=trainX,
+              y=trainY,
+              epochs=50,
+              batch_size=5)
+
+    return model
+
+
+def lstmTest(model, testX, testY):
+
+    predictions = model.evaluate(testX, testY)
+    outputs = model.predict(testX)
+    print('\n accuracy: ')
+    print(predictions[1])
+
+    return predictions[1] #outputs
+
+
 def main():
-    """
-    Returns a train, test and validation set of time series data
-    :param windowSize: how big you want the sliding window to be
-    :param trainSize: size of training set, default 0.7
-    :param testSize: size of testing set, default 0.15
-    :param validationSize: side of validation set, default 0.15
-    :return:
-    """
-
-    WINDOW = 10
-    PREDICTION = 10
-
     ## Read in participant data files
     p1 = importCSV('p1.csv')
     p2 = importCSV('p2.csv')
@@ -233,24 +203,140 @@ def main():
     p24 = importCSV('p24.csv')
     p25 = importCSV('p25.csv')
 
-    data = pd.concat(
-        [p1, p2, p3, p4, p5, p6, p7, p9, p10, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25],
-        axis=0)
-    data = data.drop(['Unnamed: 0', 'C', 'timestamp', 'date', 'PP', 'ElapsedTime', 'Condition'], axis=1)
-    data = data[~data['label'].isin(['rest'])]
-    data = data.replace({'no stress': 0, 'interruption': 1, 'time pressure': 2})
-
-    ###############################################################
-    ## Use to just test on whichever three participants you want ##
-    ###############################################################
+    ############################################################
+    ## Plots participant 21's data in disjunct condition sets ##
+    ############################################################
     """
-    train = data[~data['subject'].isin(['p2', 'p4', 'p6'])] 
-    test = data[data['subject'].isin(['p2', 'p4', 'p6'])]
-    
+    noStress = p21[~p21['label'].isin(['1','2'])]
+    interruptionStress = p21[~p21['label'].isin(['0','2'])]
+    pressureStress = p21[~p21['label'].isin(['0','1'])]
+
+    ## Make sure you comment out the drop labels in importCSV or else you get an error plotting with the timestamp
+    plt.plot(noStress['timestamp'], noStress['SCL'], c='g', label='neutral')
+    plt.plot(interruptionStress['timestamp'], interruptionStress['SCL'], label='interruption')
+    plt.plot(pressureStress['timestamp'], pressureStress['SCL'], label='pressure')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    """
+
+    ## Set window and prediction parameters to play around with
+    WINDOW = 10
+    PREDICTION = 10
+
+
+    ## Combine all the participants into one DataFrame
+    data = pd.concat([p1, p2, p3, p4, p5, p6, p7, p9, p10, p12,
+                      p13, p14, p15, p16, p17, p18, p19,
+                      p20, p21, p22, p23, p24, p25],
+                     axis=0)
+
+    ######################################################
+    ## Within-participant training, testing and gaphing ##
+    ######################################################
+    """
+    ## Train test split of all data
+    #train, test = train_test_split(data, test_size=0.3, random_state=42)
+
+    ## Train test split of neutral data
+    #data = data[~data['label'].isin(['1', '2'])]
+    #train, test = train_test_split(data, test_size=0.3, random_state=42)
+
+    ## Train test split of stress condition data
+    #data = data[~data['label'].isin(['0'])]
+    #train, test = train_test_split(data, test_size=0.3, random_state=42)
+
+    ## Makes time series data from the desired data set
     trainX, trainY = timeSeries(train, WINDOW, PREDICTION)
     testX, testY = timeSeries(test, WINDOW, PREDICTION)
+
+    ## Trains model
+    model = lstmTrain(WINDOW, PREDICTION, trainX, trainY)
+
+    ## Makes predictions and outputs accuracy
+    predictions = lstmTest(model, testX, testY)
+
+
+    ## Initialize for plotting predictions
+    input = []
+    predicted = []
+    true = []
+
+    ## Get values to plot
+    for i in range(WINDOW):
+        input.append(testX[0][i][1])
+
+    for i in range(PREDICTION):
+        predicted.append(predictions[0][i][1])
+        true.append(testY[0][i][1])
+
+    ## Setting X-labels for predictions
+    x1 = [0,1,2,3,4,5,6,7,8,9]
+    x2 = [10,11,12] #,13,14,15,16,17,18,19]
+
+    ## Plot
+    plt.plot(x1, input, c='g', label='input' )
+    plt.scatter(x2, predicted, c='b', label='predicted')
+    plt.scatter(x2, true, c='r', label='true')
+    plt.legend(loc='upper left')
+    plt.show()
+    """
+
+    #######################################################
+    ## Unseen participant training, testing and graphing ##
+    #######################################################
+    """
+    #participantList = ['p1', 'p2', 'p3', 'p4']
+    #participantList = ['p15', 'p16', 'p17', 'p18']
+    participantList = ['p20', 'p21', 'p22', 'p23']
     
-    simpleLSTM(WINDOW, PREDICTION, trainX, trainY, testX, testY)
+    ## Train test split of all data 
+    #train = data[~data['subject'].isin(participantList)]
+    #test = data[data['subject'].isin(participantList)]
+
+    ## Train test split of neutral data
+    #data = data[~data['label'].isin(['1', '2'])]
+    #train = data[~data['subject'].isin(participantList)]
+    #test = data[data['subject'].isin(participantList)]
+
+    ## Train test split of stress condition data
+    data = data[~data['label'].isin(['0'])]
+    train = data[~data['subject'].isin(participantList)]
+    test = data[data['subject'].isin(participantList)]
+
+    ## Makes time series data from the desired data set
+    trainX, trainY = timeSeries(train, WINDOW, PREDICTION)
+    testX, testY = timeSeries(test, WINDOW, PREDICTION)
+
+    ## Trains model
+    model = lstmTrain(WINDOW, PREDICTION, trainX, trainY)
+
+    ## Makes predictions and outputs accuracy
+    predictions = lstmTest(model, testX, testY)
+
+    ## Initialize for plotting predictions
+    input = []
+    predicted = []
+    true = []
+
+    ## Get values to plot
+    for i in range(WINDOW):
+        input.append(testX[0][i][1])
+
+    for i in range(PREDICTION):
+        predicted.append(predictions[0][i][1])
+        true.append(testY[0][i][1])
+
+    ## Setting X-labels for predictions
+    x1 = [0,1,2,3,4,5,6,7,8,9]
+    x2 = [10,11,12,13,14,15,16,17,18,19]
+    
+    ## Plot
+    plt.plot(x1, input, c='g', label='input' )
+    plt.scatter(x2, predicted, c='b', label='predicted')
+    plt.scatter(x2, true, c='r', label='true')
+    plt.legend(loc='upper left')
+    plt.show()
     """
 
     ###############################################################
@@ -286,11 +372,12 @@ def main():
         trainX, trainY = timeSeries(train, WINDOW, PREDICTION)
         testX, testY = timeSeries(test, WINDOW, PREDICTION)
 
-        prediction = simpleLSTM(WINDOW, PREDICTION, trainX, trainY, testX, testY)
-
+        model = lstmTrain(WINDOW, PREDICTION, trainX, trainY)
+        prediction = lstmTest(model, testX, testY)
+        
         mses.append(prediction)
 
-    meses = np.array(mses)
+    mses = np.array(mses)
     print(mses.mean())
 
     return
